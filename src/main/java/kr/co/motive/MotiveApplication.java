32d471c182e24597c7a72d9fdab60b84c99e647f
jaeyoung <jaeyoung1375@naver.com>
2025-07-01 21:56:39 +0900

    초기 적재

---
+++ src/main/java/kr/co/motive/MotiveApplication.java
@@ -0,0 +1,13 @@
+package kr.co.motive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/motive/MotiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.motive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
